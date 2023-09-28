a573ec565fa10212f5fdc1733badcc39df7ad0ae
ModernMorven <modernmorven@gmail.com>
2023-09-29 04:14:30 +0500

    change
    backend

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -24,11 +24,11 @@ app.use((req, res, next) => {
 const corsOptions = {
   origin: [ "https://modernmorven.com","https://www.modernmorven.com","https://modernmorven.com", "https://185.201.9.59"],
 };
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.use(cors(corsOptions)); // Use this middleware to handle CORS
 
