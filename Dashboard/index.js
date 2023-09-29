b6495184f91d7ff204abd8159b3a67cd324c6b0d
ModernMorven <modernmorven@gmail.com>
2023-09-29 20:57:52 +0500

    final
    chnage

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -92,7 +92,7 @@ app.use(express.json());
 const ProductModel = require("./models/Addproduct");
 
 const storage = multer.diskStorage({
-  destination: '../modernmorven/public/ProductData', // Change this to your desired destination
+  destination: 'var/www/ModernMorven/modernmorven/public/ProductData', // Change this to your desired destination
   filename: (req, file, cb) => {
     // Generate a unique filename for each image
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
