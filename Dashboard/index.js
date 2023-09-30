d61463c3c0d5899136c70642891f0f7ec95b4d95
ModernMorven <modernmorven@gmail.com>
2023-09-30 16:25:13 +0500

    update
    cors blocking issue

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -10,12 +10,12 @@ app.use(express.json());
 const cors = require("cors");
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', "https://www.modernmorven.com");
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, ProductForm, Accept');
   next();
 });
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', "https://185.201.9.59");
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,ProductForm, Accept');
   next();
 });
 
