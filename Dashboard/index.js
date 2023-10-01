ec1fd07a32f23199121ceab0339b52d5b4f2dff9
ModernMorven <modernmorven@gmail.com>
2023-10-01 16:10:53 +0500

    chnage in css file
    font size change

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 
 const corsOptions = {
-  origin: [ "https://modernmorven.com","https://www.modernmorven.com","https://www.media.modernmorven.com","https://modernmorven.com", "https://185.201.9.59","https://127.0.0.1:3000"],
+  origin: [ "https://modernmorven.com","https://www.modernmorven.com","https://www.media.modernmorven.com","https://modernmorven.com", "https://185.201.9.59","https://127.0.0.1"],
 };
 
 
